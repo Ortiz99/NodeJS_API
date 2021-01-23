243c4fdd4d8b0325c2e8baa3b580e594a66b7823
Ortiz99 <rortizro@ibero.edu.co>
2021-01-23 12:43:02 -0500

    MongoDB Connection

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 const mongoose = require('mongoose');
 const port = 3000;
+
+//MongoDB Connection
+mongoose.connect('mongodb://localhost:27017/BIT', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err, res) => {
+    var message = err ? 'Connection Error' : 'Connection Stablished'
+    console.log(message);
+});
