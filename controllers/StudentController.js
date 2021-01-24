9d977d74facf76468b25b387deea062aa02f0722
Ortiz99 <rortizro@ibero.edu.co>
2021-01-23 20:29:32 -0500

    CRUD

--- controllers/StudentController.js
+++ controllers/StudentController.js
@@ -35,6 +35,103 @@ function create(req, res) {
     });
 }
 
+function update(req, res) {
+    var parameters = req.body;
+    var id_student = req.params.id;
+
+    User.findByIdAndUpdate(id_student, parameters, { new: true }, (err, userUpdated) => {
+        if (err) {
+            res.status(500).send({
+                statusCode: 500,
+                message: 'Server Error',
+                error: err
+            })
+        } else {
+            if (!userUpdated) {
+                res.status(400).send({
+                    statusCode: 400,
+                    message: "Error Updating"
+                })
+            } else {
+                res.status(200).send({
+                    statusCode: 200,
+                    message: 'Student Updated Successfully',
+                    user: userUpdated
+                })
+            }
+        }
+    });
+}
+
+function remove(req, res) {
+    var student_id = req.params.id;
+
+    User.findByIdAndDelete(student_id, (err, userRemoved) => {
+        if (err) {
+            res.status(500).send({
+                statusCode: 500,
+                message: 'Server Error',
+                error: err
+            })
+        } else {
+            if (!userRemoved) {
+                res.status(400).send({
+                    statusCode: 400,
+                    message: 'Error Removing Student'
+                })
+            } else {
+                res.status(200).send({
+                    statusCode: 200,
+                    message: 'Student Deleted Successfully',
+                    userRemoved: userRemoved
+                })
+            }
+        }
+
+    })
+}
+
+function getAllStudents(req, res) {
+    User.find({}, (err, students) => {
+        if (err) {
+            res.status(500).send({
+                statusCode: 500,
+                message: 'Server Error'
+            })
+        } else {
+            res.status(200).send({
+                statusCode: 200,
+                message: 'Users',
+                students: students
+            })
+        }
+    });
+
+}
+
+function getStudent(req, res) {
+    student_id = req.params.id;
+
+    User.findById(student_id, (err, user) => {
+        if (err) {
+            res.status(500).send({
+                statusCode: 500,
+                message: 'Server Error',
+                error: err
+            })
+        } else {
+            res.status(200).send({
+                statusCode: 200,
+                user: user
+            })
+        }
+    })
+}
+
 module.exports = {
-    create
+    create,
+    update,
+    remove,
+    getAllStudents,
+    getStudent
 }
--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -2,18 +2,32 @@ const express = require('express');
 const api = express.Router();
 const StudentController = require('../controllers/StudentController');
 
+//ROUTES
+
+
 /**
  * GET
  */
+api.get('/students', StudentController.getAllStudents);
+api.get('/student/:id', StudentController.getStudent);
 
-api.get('/', () => {
-    console.log('juju');
-})
 
 /**
  * POST
  */
 api.post('/saveStudent', StudentController.create);
+
+
+/**
+ * PUT
+ */
+api.put('/updateStudent/:id', StudentController.update);
+
+
+/**
+ * DELETE
+ */
+api.delete('/deleteStudent/:id', StudentController.remove)
 
 
 module.exports = api;
